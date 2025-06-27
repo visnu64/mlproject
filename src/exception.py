a949d6191f93759ec6dbfa78a2e0fe9760cb2bdc
unknown <visnus12a22223@gmail.com>
2025-06-27 21:56:34 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -3,9 +3,9 @@ import sys
 def error_message_details(error,error_detail:sys):
     _,_,exe_tb=error_detail.exc_info()
     file_name=exe_tb.tb_frame.f_code.co_filename
-    error_message="Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format()
+    error_message="Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
     file_name,exe_tb.tb_lineno,str(error)
-
+    )
     return error_message
 
 class customexception(Exception):
--- src/logger.py
+++ src/logger.py
@@ -2,11 +2,11 @@ import logging
 import os
 from datetime import datetime
 
-log_file=f"{datetime.now().strftime('%m_%D_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",log_file)
-os.makedirs(logs_path,exist_ok=True)
+log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
 
-log_file_path=os.path.join(logs_path,log_file)
+log_file_path = os.path.join(logs_path, log_file)
 
 logging.basicConfig(
     filename=log_file_path,
